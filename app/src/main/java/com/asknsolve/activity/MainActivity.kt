752e9611ed1686839af0eb92440d40287c047d09
cava <cava@kaist.ac.kr>
2021-07-16 16:55:43 +0900

    값 주며 SubActivity.kt로 이동하는 것까지 됨

--- app/src/main/java/com/asknsolve/activity/MainActivity.kt
+++ app/src/main/java/com/asknsolve/activity/MainActivity.kt
@@ -4,24 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.asknsolve.activity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        if(resultCode == RESULT_OK){
+//            when(requestCode){
+//                99 -> {
+//                    val message = data?.getStringExtra("returnValue")
+//                    Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+//                }
+//            }
+//        }
+//    }
 
-        if(resultCode == RESULT_OK){
-            when(requestCode){
-                99 -> {
-                    val message = data?.getStringExtra("returnValue")
-                    Toast.makeText(this,message,Toast.LENGTH_LONG).show()
-                }
-            }
-        }
-    }
+//    val getAction = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+//        if(it.resultCode == RESULT_OK){
+//            // There are no request Codes
+//            val message = it.data?.getStringExtra("returnValue")
+//            Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+//        }
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +39,11 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, SubActivity::class.java)
         intent.putExtra("from1","Hello Bundle")
         intent.putExtra("from2", 2021)
-        binding.btnStart.setOnClickListener { startActivityForResult(intent, 99) }
+        binding.btnStart.setOnClickListener {
+            startActivity(intent)
+//          startActivityForResult(intent, 99)
+//            getAction.launch(intent)
+        }
 
     }
 }
--- app/src/main/java/com/asknsolve/activity/SubActivity.kt
+++ app/src/main/java/com/asknsolve/activity/SubActivity.kt
@@ -10,14 +10,6 @@ class SubActivity : AppCompatActivity() {
 
     val binding by lazy { ActivitySubBinding.inflate(layoutInflater) }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == RESULT_OK){
-            val message = data?.getStringExtra("returnValue")
-            Toast.makeText(this, message,Toast.LENGTH_LONG).show()
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -25,16 +17,16 @@ class SubActivity : AppCompatActivity() {
         binding.to1.text = intent.getStringExtra("from1")
         binding.to2.text = "${intent.getIntExtra("from2", 0)}"
 
-        binding.btnClose.setOnClickListener {
-            val returnIntent = Intent()
-            returnIntent.putExtra("retrunValue",binding.editMessage.text.toString())
-            setResult(RESULT_OK, returnIntent)
-            finish()
-        }
+//        binding.btnClose.setOnClickListener {
+//            val returnIntent = Intent()
+//            returnIntent.putExtra("retrunValue",binding.editMessage.text.toString())
+//            setResult(RESULT_OK, returnIntent)
+//            finish()
+//        }
 
-        val returnIntent = Intent()
-        returnIntent.putExtra("returnValue",binding.editMessage.text.toString())
-        setResult(RESULT_OK, returnIntent)
-        finish()
+//        val returnIntent = Intent()
+//        returnIntent.putExtra("returnValue",binding.editMessage.text.toString())
+//        setResult(RESULT_OK, returnIntent)
+//        finish()
     }
 }
